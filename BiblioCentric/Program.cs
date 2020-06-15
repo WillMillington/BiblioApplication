ef6fe3ab79750933b6694dc0f01dd41feaba2450
William Millington <WMillington@SpartaGlobal.com>
2020-06-15 18:43:49 +0100

    (Initial Commit) Project set-up and WPF/Business layer creation

---
+++ BiblioCentric/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BiblioCentric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
