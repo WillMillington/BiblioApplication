0ab40ef78a17bca6b9630411433bbbe3a12c3131
William Millington <WMillington@SpartaGlobal.com>
2020-06-16 10:57:24 +0100

    Created Database/Added first entry

---
+++ BiblioCentric/Model.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BiblioCentric
+{
+    class BiblioContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Biblio;Integrated Security=True; ConnectRetryCount=0");
+    }
+
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string ISBN10 { get; set; }
+        public string ISBN13 { get; set; }
+        public string Publisher { get; set; }
+        public string PublishedDate { get; set; }
+        public int? NumOfPages { get; set; }
+        public string Description { get; set; }
+        public int? Review { get; set; }
+        public int NumOfCopies { get; set; } = 1;
+        public bool? Read { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+
+        public List<Book> Books { get; } = new List<Book>();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# BiblioApplication
-This is the repository for my C# Project and Biblio Application
+# Bibliocentric Application
+#### Project Goal:
+
+> Create an application to store, view and manage book collections.
+
+##### Sprint 1
+----------------
+Prepare everything for the project so that it is ready to move into the next sprint.
+
+- Project Set-up
+- Database Creation
+- WPF and Business layer creation
+
+Retrospective
+Screenshots
+
